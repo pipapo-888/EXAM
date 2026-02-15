9ff650add202aa50905e78648ca959577c27159f
pipapo-888 <kazuya2002uk@gmail.com>
2026-02-15 17:54:04 +0900

    filter 自分用

--- Rank_03/Level01/filter.c
+++ Rank_03/Level01/filter.c
@@ -1,7 +1,30 @@
+// filter:
+// Allowed functions: read, strlen, malloc, calloc, realloc, free, printf, perror
+// ------------------------------------------------------------------------------
+
+// Write a programm taht will take one and only one argument s.
+
+// Your programm will then read from stdin and write all the content read in stdout
+// except that every occurence of s must be replaced by '*'.
+
+// For example :
+
+// ./filter bonjour
+// will behave the same way as:
+// sed 's/bonjour/*******/g'
+
+// in case of error during read or a malloc you must write "Error: " followed by
+// the error message in stderr and return 1.
+
+// $> echo 'abcdefgaaaabcdefabc' | ./filter abc | cat -e
+// ***defgaaa***def***
+
+// If the program is called whitout argument or with an empty argument or with
+// multiples arguments it must return 1.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 
 #define BUFSIZE 100000
 
@@ -9,17 +32,26 @@ int main(int argc, char **argv)
 {
 	char *buf;
 	char *str = argv[1];
-	char c;
+	int read_line = 1;
 
 	if (argc != 2 || argv[1][0] == '\0')
 	{
-		write(2, "Error: sss", 10);
+		write(2, "Error: 1 Argument Only", 10);
 		return (1);
 	}
 	buf = malloc(BUFSIZE + 1);
-	// read(0, buf, BUFSIZE);
-	while (read(0, &c, 1) > 0)
-		write(1, &c, 1);
-	printf("%s\n", str);
+
+	while (read_line > 0)
+	{
+		read_line = read(0, buf, BUFSIZE);
+		if (read_line = -1)
+			write(2, "Error:", 6);
+		buf[read_line] = '\0';
+	}
+
+	read(0, buf, BUFSIZE);
+	// while (read(0, &c, 1) > 0)
+	// 	write(1, &c, 1);
+	// printf("%s\n", str);
 	return (0);
 }
