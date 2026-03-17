c7759ecf05c21e58bda608b7164c31d6ed1ddb93
Kazuya <kazuya2002uk@gmail.com>
2026-03-17 18:20:47 +0900

    rip

--- Rank_03/Level02/rip.c
+++ Rank_03/Level02/rip.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
+// int is_valid(char *str)
+// {
+// 	int opened = 0, closed = 0;
+
+// 	for (int i = 0; str[i]; i++)
+// 	{
+// 		if (str[i] == '(')
+// 			opened++;
+// 		else if (str[i] == ')')
+// 		{
+// 			if (opened > 0)
+// 				opened--;
+// 			else
+// 				closed++;
+// 		}
+// 	}
+// 	return (opened + closed);
+// }
+
+// void dfs(char *str, int must_fix, int n_fixed, int pos)
+// {
+// 	if (must_fix == n_fixed && is_valid(str) == 0)
+// 	{
+// 		puts(str);
+// 		return ;
+// 	}
+// 	for (int i = pos; str[i]; i++)
+// 	{
+// 		if (str[i] == '(' || str[i] == ')')
+// 		{
+// 			int c = str[i];
+// 			str[i] = ' ';
+// 			dfs(str, must_fix, n_fixed + 1, i);
+// 			str[i] = c;
+// 		}
+// 	}
+// }
+
+// int	main(int ac, char **argv)
+// {
+
+// 	if (ac != 2)
+// 	{
+// 		puts("WRONG AC");
+// 		return (1);
+// 	}
+// 	int m_fix = is_valid(argv[1]);
+// 	dfs(argv[1], m_fix, 0, 0);
+
+// 	return (0);
+// }
+
 int is_valid(char *str)
 {
 	int opened = 0, closed = 0;
@@ -31,26 +83,21 @@ void dfs(char *str, int must_fix, int n_fixed, int pos)
 	{
 		if (str[i] == '(' || str[i] == ')')
 		{
-			int c = str[i];
-			str[i] = ' ';
+			char c = str[i];
+			str[i] = '_';
 			dfs(str, must_fix, n_fixed + 1, i);
 			str[i] = c;
 		}
 	}
 }
 
-int	main(int ac, char **argv)
+
+int main(int ac, char **av)
 {
+	if (ac != 2 || av[1][0] == '\0')
+		return(printf("WRONG"), 1);
+	int m_fix = is_valid(av[1]);
 
-	if (ac != 2)
-	{
-		puts("WRONG AC");
-		return (1);
-	}
-	int m_fix = is_valid(argv[1]);
-	dfs(argv[1], m_fix, 0, 0);
+	dfs(av[1], m_fix, 0, 0);
 
-	// printf("%d", m_fix);
-
-	return (0);
 }
