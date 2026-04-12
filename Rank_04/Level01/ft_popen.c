efbd2b7198dc303ad4be8c3a5ac15e848c64f9a0
pipapo888 <kazuya2002uk@gmail.com>
2026-04-12 18:32:38 +0900

    int

--- Rank_04/Level01/ft_popen.c
+++ Rank_04/Level01/ft_popen.c
@@ -1,1 +1,26 @@
-d-
+#include <stdio.h>
+
+// void *ft_popen(const char *command, const char *type)
+// {
+
+
+
+
+// 	return 
+// }
+
+// int ft_popen(const char *file, char *const argv[], char type)
+// {
+
+
+
+// }
+
+int	main() {
+	int	fd = ft_popen("ls", (char *const []){"ls", NULL}, 'r');
+	dup2(fd, 0);
+	fd = popen("grep", (char *const []){"grep", "c", NULL}, 'r');
+	char	*line;
+// 	while ((line = get_next_line(fd)))
+// 		printf("%s", line);
+}
