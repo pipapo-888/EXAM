ef58d21cb98a0cb45bda6d44c773677b3ea11ca8
pipapo888 <kazuya2002uk@gmail.com>
2026-04-28 14:27:41 +0900

    かけるかテスト

--- Rank_04/Level02/vbc/vbc.c
+++ Rank_04/Level02/vbc/vbc.c
@@ -3,20 +3,20 @@
 
 static char *pos;
 
-static void	error(char c)
+void error(char c)
 {
 	if (c == '\0')
 		printf("Unexpected end of input\n");
-	else
+	else 
 		printf("Unexpected token '%c'\n", c);
 	exit(1);
 }
 
-static int	parse_expr(void);
+int parse_expr();
 
-static int	parse_factor(void)
+int parse_factor()
 {
-	int	val;
+	int val;
 
 	if (*pos == '(')
 	{
@@ -31,14 +31,14 @@ static int	parse_factor(void)
 		val = *pos - '0';
 		pos++;
 	}
-	else
+	else 
 		error(*pos);
 	return (val);
 }
 
-static int	parse_term(void)
+int parse_term()
 {
-	int	val;
+	int val;
 
 	val = parse_factor();
 	while (*pos == '*')
@@ -46,12 +46,13 @@ static int	parse_term(void)
 		pos++;
 		val *= parse_factor();
 	}
+
 	return (val);
 }
 
-static int	parse_expr(void)
+int parse_expr()
 {
-	int	val;
+	int val;
 
 	val = parse_term();
 	while (*pos == '+')
@@ -59,19 +60,94 @@ static int	parse_expr(void)
 		pos++;
 		val += parse_term();
 	}
-	return (val);
+	return (val);	
 }
 
-int	main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	int	result;
+	if (ac != 2)
+		return 1;
+	pos = av[1];	
 
-	if (ac != 2 || av[1][0] == '\0')
-		return (1);
-	pos = av[1];
-	result = parse_expr();
+	int ans = parse_expr();
 	if (*pos != '\0')
 		error(*pos);
-	printf("%d\n", result);
+	printf("%d\n", ans);
+
 	return (0);
 }
+
+// static char *pos;
+
+// static void	error(char c)
+// {
+// 	if (c == '\0')
+// 		printf("Unexpected end of input\n");
+// 	else
+// 		printf("Unexpected token '%c'\n", c);
+// 	exit(1);
+// }
+
+// static int	parse_expr(void);
+
+// static int	parse_factor(void)
+// {
+// 	int	val;
+
+// 	if (*pos == '(')
+// 	{
+// 		pos++;
+// 		val = parse_expr();
+// 		if (*pos != ')')
+// 			error(*pos);
+// 		pos++;
+// 	}
+// 	else if (*pos >= '0' && *pos <= '9')
+// 	{
+// 		val = *pos - '0';
+// 		pos++;
+// 	}
+// 	else
+// 		error(*pos);
+// 	return (val);
+// }
+
+// static int	parse_term(void)
+// {
+// 	int	val;
+
+// 	val = parse_factor();
+// 	while (*pos == '*')
+// 	{
+// 		pos++;
+// 		val *= parse_factor();
+// 	}
+// 	return (val);
+// }
+
+// static int	parse_expr(void)
+// {
+// 	int	val;
+
+// 	val = parse_term();
+// 	while (*pos == '+')
+// 	{
+// 		pos++;
+// 		val += parse_term();
+// 	}
+// 	return (val);
+// }
+
+// int	main(int ac, char **av)
+// {
+// 	int	result;
+
+// 	if (ac != 2 || av[1][0] == '\0')
+// 		return (1);
+// 	pos = av[1];
+// 	result = parse_expr();
+// 	if (*pos != '\0')
+// 		error(*pos);
+// 	printf("%d\n", result);
+// 	return (0);
+// }
